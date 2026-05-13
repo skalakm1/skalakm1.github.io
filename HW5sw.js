b9bac369ab57c5871328f82a5785671864b6ce60
skalakm1 <skalakm1@my.smccd.edu>
2026-05-12 22:10:41 -0700

    Add files via upload

---
+++ HW5sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'CacheA';
+const urlsToCache = [
+    '/',
+    '/HW5.html',
+    '/HW4.css',
+    '/HW5sw.js'
+];
+
+// Install event: Caches static assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event: Intercepts network requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {                     // Cache hit - return response
+                    return response;
+                }
+                return fetch(event.request);        // No cache hit - fetch from network
+            })
+    );
+});
+
+// Activate event: Cleans up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
